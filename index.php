e73de2862e74feee21604d35a210b37aa9b193b6
stefan_s <stendek55@gmail.com>
2019-05-14 17:29:42 +0200

    ordnerstruktur erstellt und dateien mit testdaten befüllt

--- index.php
+++ index.php
@@ -3,3 +3,19 @@ echo "Hallo Welt!";
 echo "Hallo Welt! - Zeile zwei";
 
 echo "ahoi ahoi";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>fu_ma_si</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+
+    <h1>fu_ma_si</h1>
+    <h2>tobi_stefan_tobi</h2>
+
+</body>
+</html>

a257893e7090b5f0b9f302e5436d24e0b48460e4
stefan_s <stendek55@gmail.com>
2019-06-01 14:51:41 +0200

    einrückung in home korrigiert

--- home.php
+++ home.php
@@ -13,40 +13,40 @@ nur im content werden die dinge geschrieben die man sehen soll
 der rest wird ja aus den anderen templates gezogen-->
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>fu_ma_si</title>
-    <link rel="stylesheet" href="style/style.css">
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>fu_ma_si</title>
+        <link rel="stylesheet" href="style/style.css">
+    </head>
+    <body>
 
-<!--nur in dieser datei wird der header geschrieben-->
-<?php include_once "./templates/haeder.php"; ?>
+        <!--nur in dieser datei wird der header geschrieben-->
+        <?php include_once "./templates/haeder.php"; ?>
 
-<div id="info_and_content">
+        <div id="info_and_content">
 
-    <!-- in dem div welche in der info.php liegt
-    kommt quasi nur das infozeugs-->
-    <?php include_once "./templates/info.php"; ?>
+            <!-- in dem div welche in der info.php liegt
+            kommt quasi nur das infozeugs-->
+            <?php include_once "./templates/info.php"; ?>
 
-    <!--nur in diesem div werden je nach seite die inhalte geändert
-    und nirgends sonst
-    demzufolge gibt es eine kopie dieser ganzen seite wo je nachdem nur
-    hier die inhalte für
-    -training
-    -spiel
-    -tabelle
-    -einkauf
-    -...
-    geändert werden-->
-    <div id="home">
-        content home
-    </div>
+            <!--nur in diesem div werden je nach seite die inhalte geändert
+            und nirgends sonst
+            demzufolge gibt es eine kopie dieser ganzen seite wo je nachdem nur
+            hier die inhalte für
+            -training
+            -spiel
+            -tabelle
+            -einkauf
+            -...
+            geändert werden-->
+            <div id="home">
+                content home
+            </div>
 
-</div>
+        </div>
 
-<!--und in dieser nur der footer-->
-<?php include_once "./templates/footer.php"; ?>
+        <!--und in dieser nur der footer-->
+        <?php include_once "./templates/footer.php"; ?>
 
-</body>
+    </body>
 </html>

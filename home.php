582c963206332ba97f42a98b3fb857233b4b10b5
Tobias Kinder <tk.tobiki@gmail.com>
2019-06-01 13:53:26 +0200

    Homeentwurf

--- home.php
+++ home.php
@@ -1,32 +1,120 @@
-<?php
-    session_start();
+<!doctype html>
 
-    $_SESSION['username'] = $_POST['name'];
-    $_SESSION['passwort'] = $_POST['passwort'];
-?>
+<html lang="de">
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>fu_ma_si</title>
-        <link rel="stylesheet" href="style/style.css">
+  <head>
+
+    <!-- Required meta tags -->
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+ 
+
+<title>Bootstrap Training</title>
+
+ 
+
+<link href="https://fonts.googleapis.com/css?family=Rajdhani:300,500,600|Source+Sans+Pro:400,700" rel="stylesheet">
+
+ 
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+ 
+
+    <!-- Mein CSS -->
+
+ 
+
+    <link rel="stylesheet" href="http://localhost/css/style.css">
+
+ 
+
     </head>
+
     <body>
 
-        <?php include_once "./templates/haeder.php"; ?>
+ <div class="uibereich">
+ <div class="kalender"></div>
+  <div class="navbereich"></div>
+</div>
 
-        <div id="info_and_content">
+<div class="main">
+  <div class="row">
+    <div class="a"></div>
+    <div class="b"></div>
+</div>
+</div>
 
-            <?php include_once "./templates/info.php"; ?>
 
-            <div id="home">
-                content home
-            </div>
+ <div class="footer"></div>
 
-        </div>
+</div>
 
-        <?php include_once "./templates/footer.php"; ?>
+<style type="text/css">
+body {
+		background-image: url("https://answers.ea.com/ea/attachments/ea/fifa-18-technical-issues-en/1347/1/9-21-2017_2-12-52_PM.png");
+}
 
-    </body>
+.uibereich {
+	width: 1300px;
+	margin-left: 250px;
+	margin-top: 25px;
+	margin-bottom: 3px;
+}
+
+.kalender {
+	background-color: #ccc;
+	height: 140px;
+background-color: #ccc;
+}
+
+.navbereich {
+	background-color: #999;
+	height: 70px;
+
+}
+.main {
+	width: 1300px;	
+	margin-left: 265px;
+}
+.a {
+
+	height: 640px;
+	width: 280px;
+background-color: #ccc;
+}
+
+.b {
+	height: 640px;
+	width: 1020px;
+	background-color: #999;
+}
+.footer {
+	background-color: #888;
+	height: 80px;
+	margin-left: 250px;
+	width: 1300px;
+background-color: #888;
+}
+
+</style>
+
+
+    <!-- Optional JavaScript -->
+
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+  </body>
+
 </html>

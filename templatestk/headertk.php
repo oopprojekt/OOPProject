1c90bec960d585569e75a46249b8631ebcf32df8
Tobias Kinder <tk.tobiki@gmail.com>
2019-06-03 11:11:21 +0200

    kalendergrid

--- templatestk/headertk.php
+++ templatestk/headertk.php
@@ -1,1 +1,36 @@
-header
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm cntl">
+      One of three columns
+    </div>
+    <div class="col-sm cntr">
+      One of three columns
+    </div>
+        <div class="col-sm cntr">
+      One of three columns
+    </div>
+        <div class="col-sm cntr">
+      One of three columns
+    </div>
+        <div class="col-sm cntr">
+      One of three columns
+    </div>
+  </div>
+</div>
+
+<style type="text/css">
+		.cntl {
+		background-color: #888;
+		margin-right: 5px;
+		margin-left: 270px;
+		margin-top: 5px;
+		height: 140px;
+	}
+
+		.cntr {
+		background-color: #888;
+		margin-right: 5px;
+		margin-top: 5px;
+		height: 140px;
+	}
+</style>

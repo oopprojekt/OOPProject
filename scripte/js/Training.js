30366132c18266c161f15df92b01f5b91785c08f
stefan_s <stendek55@gmail.com>
2019-06-11 20:04:17 +0200

    eine erste js-klasse als schablone

---
+++ scripte/js/Training.js
@@ -0,0 +1,17 @@
+class Training
+{
+    constructor(foo)
+    {
+        this.foo = foo;
+    }
+
+    set_foo(foo)
+    {
+        this.foo = foo;
+    }
+
+    get_foo()
+    {
+        return this.foo;
+    }
+}

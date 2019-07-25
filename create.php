05e77eb3c4fce2e5f445a02b4dd82167cb543214
Tobias Kinder <tk.tobiki@gmail.com>
2019-07-25 22:11:58 +0200

    alte Inhalte in create eingefügt, früher stand

--- create.php
+++ create.php
@@ -11,7 +11,7 @@
       <div class="page-container flex-y">
          <!-- First row -->
          <div class="flex-x mt-3">
-            <div class="first-block grey-block"></div>
+            <div class="first-block grey-block">Starte deine Karriere!</div>
             <div class="flex-y flex-1 mr-10">
                <div class="white-space">
                </div>
@@ -24,16 +24,68 @@
          <div class="flex-x flex-1 mb-3">
             <div class="flex-y flex-1">
                <div class="flex-1 flex-x mr-6 mt-3">
-                  <div class="grey-block third-block"></div>
-                  <div class="grey-block flex-1 left-border-none"></div>
+                  <div class="grey-block third-block">
+
+
+                                <label for="usr">Vorname:</label>
+                                <input type="text" class="form-control trainer_firstnameform  " id="usr"><br/>
+
+                                <label for="usr2">Nachname:</label>
+                                <input type="text" class="form-control trainer_firstnameform " id="usr2">
+
+
+                  </div>
+                  <div class="grey-block flex-1 left-border-none">
+                     
+
+                            <img id="coach_a" src="bilder/trainer1.jpg" width="32%" height="">
+                            <img id="coach_b" src="bilder/trainer2.jpg" width="32%" height="">
+                            <img id="coach_c" src="bilder/trainer3.jpg" width="32%" height="">
+      
+                            <img id="coach_d" src="bilder/trainer4.jpg" width="32%" height="">
+                            <img id="coach_e" src="bilder/trainer5.jpg" width="32%" height="">
+                            <img id="coach_f" src="bilder/trainer6.jpg" width="32%" height="">
+                        
+                  </div>
                </div>
                <div class="flex-x third-row mtr-6">
                   <div class="grey-block flex-1"></div>
-                  <div class="grey-block last-block left-border-none"></div>
+                  <div class="grey-block last-block left-border-none">
+                     <a href="#">Karriere starten </a>
+                  </div>
                </div>
             </div>
-            <div class="right-last-block grey-block"></div>
+            <div class="right-last-block"><img id="trainer_gross" width="110%" height="auto"></div>
          </div>
       </div>
+
+
+
+<script>
+
+    let img_coach = document.getElementById("trainer_gross");
+    img_coach.src = "./bilder/coach_b.png";
+
+    document.addEventListener('click', (e) => {
+
+        let id_img = e.target.id;
+        let str_coach = id_img.substr(0, 5);
+
+
+        if ("coach" === str_coach) {
+            img_coach.src = "./bilder/" + id_img + ".png";
+        }
+
+    }, false);
+
+</script>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
    </body>
 </html>

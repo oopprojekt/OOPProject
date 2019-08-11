c3917da4f8309ee854934ec7204a12b80495426c
Stefan Senftleben <ssenftleben@eos-uptrade.de>
2019-08-11 12:36:30 +0200

    pseudocode für datafixtures

---
+++ scripte/php/Fixtures.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ssenftleben
+ * Date: 11.08.19
+ * Time: 12:25
+ */
+
+class Fixtures
+{
+
+    //eine globale variable für die DB klasse ist aber noch null
+
+
+    //im construktor wird ein objekt der datenbankklasse erzeugt
+
+
+    //eine public methode worin dann alles passiert die bekommt am besten einen parameter
+    //der zum ansprechen der teams benötigt wird
+
+        /*in der methode:
+        - für jden wert der für die spieler benötigt wird erzeugst du eine variable
+        - name, alter, fähigkeiten, ...
+
+        - dann kommt eine normale for-schleife mit der anzahl der gewünschten spieler
+        */
+
+
+
+}

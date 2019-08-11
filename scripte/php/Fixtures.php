987f39c30d71bf51b1a14d837814709a66a1f9f9
stefan_s <stendek55@gmail.com>
2019-08-11 23:18:13 +0200

    datafixtures objektorientiert umgesetzt

--- scripte/php/Fixtures.php
+++ scripte/php/Fixtures.php
@@ -1,57 +1,68 @@
 <?php
 include_once "Config.php";
+include_once "DB.php";
 
+/**
+ * Class Fixtures
+ */
 class Fixtures
 {
-    //eine globale variable für die DB klasse ist aber noch null
     public $connection = null;
 
-    //im construktor wird ein objekt der datenbankklasse erzeugt
+    /**
+     * Fixtures constructor.
+     */
     function __construct()
     {
-        $this->connection  = mysqli_connect(Config::$HOST, Config::$NUTZER, Config::$PASSWORT, Config::$DB);
+        $this->connection = new DB("else@gmx.com");
     }
-    public function execute($sql)
+
+    /**
+     * zum leeren des alten standes
+     */
+    private function delete_fixtures()
     {
-        return mysqli_query($this->connection, $sql);
+        $sql = "DELETE FROM tbl_spieler;";
+        $this->connection->execute($sql);
     }
 
-    //eine public methode worin dann alles passiert die bekommt am besten einen parameter
-    //der zum ansprechen der teams benötigt wird
+    /**
+     * damit die id's mit 1 beginnen
+     */
+    private function set_increment()
+    {
+        $sql = "ALTER TABLE tbl_spieler AUTO_INCREMENT = 1;";
+        $this->connection->execute($sql);
+    }
 
-        /*in der methode:
-        - für jden wert der für die spieler benötigt wird erzeugst du eine variable
-        - name, alter, fähigkeiten, ...
-
-        - dann kommt eine normale for-schleife mit der anzahl der gewünschten spieler
-        */
+    /**
+     * schreibt für alle teams jeweils 11 spieler
+     */
     public function set_player_data()
     {
-        $set_increment = 'ALTER TABLE tbl_spieler AUTO_INCREMENT = 1;';
-        $this->execute($set_increment);
-        for ($team_id = 3; $team_id <= 20; $team_id++) {
+        $this->delete_fixtures();
+        $this->set_increment();
+
+        for ($team_id = 3; $team_id <= 20; $team_id++)
+        {
             //spieler werden angelegt
-            for ($i = 1; $i <= 11; $i++) {
+            for ($i = 1; $i <= 11; $i++)
+            {
                 $alter = rand(16,49);
                 $position = rand(1,10);
                 $ausdauer = rand(1,100);
                 $technik = rand(1,100);
                 $torgefahr = rand(1,100);
                 $zweikampf = rand(1,100);
-                $rote_karte = 0;
-                $gelbe_karte = 0;
-                $verletzt = 0;
                 $preis = rand(10000,100000);
-                $tore = 0;
-                $nummer = 0;
+
                 $sql = "INSERT INTO `tbl_spieler` (`spl_vorname`, `spl_nachname`, `spl_fs_team`, `spl_alter`, `spl_fs_position`, `spl_ausdauer`, `spl_technik`, `spl_torgefahr`, `spl_zweikampf`, `spl_rote`, `spl_gelbe`, `spl_verletzt`, `spl_preis`, `spl_tore`, `spl_nummer`) 
-    VALUES ('spl_" . $i . "_vorname', 'spl_" . $i . "_nachname',' " . $team_id . "','" . $alter . "', '" . $position . "', '" . $ausdauer . "', '" . $technik . "', '" . $torgefahr . "', '" . $zweikampf . "', '0', '0', '0', '" . $preis . "', '1', '" . $i . "');";
-                //INSERT INTO `tbl_spieler` (`spl_id`, `spl_vorname`, `spl_nachname`, `spl_fs_team`, `spl_alter`, `spl_fs_position`, `spl_ausdauer`, `spl_technik`, `spl_torgefahr`, `spl_zweikampf`, `spl_rote`, `spl_gelbe`, `spl_verletzt`, `spl_preis`, `spl_tore`, `spl_nummer`)
-                // VALUES (NULL, 'hans', 'gruber', '15', '35', '1', '100', '100', '100', '100', '0', '0', '0', '1', '1', '1')
-                //echo($sql); echo "<br>";
-                $this->execute($sql);
+    VALUES ('spl_" . $i . "_vorname', 'spl_" . $team_id . "_nachname',' " . $team_id . "','" . $alter . "', '" . $position . "', '" . $ausdauer . "', '" . $technik . "', '" . $torgefahr . "', '" . $zweikampf . "', '0', '0', '0', '" . $preis . "', '0', '" . $i . "');";
+
+                $this->connection->execute($sql);
             }
         }
-    }
 
+        echo "testdummys der spieler wurden geschrieben";
+    }
 }
--- testing.php
+++ testing.php
@@ -56,45 +56,8 @@ echo "trallali";
 
 <?php
 
-    include_once "./scripte/php/DB.php";
-
-    $foo = new DB("else@gmx.com");
-
-    echo $foo->get_team_id("else@gmx.com");
-    echo "<br><br>...";
-
-        $con = mysqli_connect("localhost","root","root","fumasi");
-        $set_increment = 'ALTER TABLE tbl_spieler AUTO_INCREMENT = 1;';
-        mysqli_query($con, $set_increment);
-        for ($team_id = 3; $team_id <= 20; $team_id++) {
-            //spieler werden angelegt
-            for ($i = 1; $i <= 11; $i++) {
-                $alter = rand(16,49);
-                $position = rand(1,10);
-                $ausdauer = rand(1,100);
-                $technik = rand(1,100);
-                $torgefahr = rand(1,100);
-                $zweikampf = rand(1,100);
-                $rote_karte = 0;
-                $gelbe_karte = 0;
-                $verletzt = 0;
-                $preis = rand(10000,100000);
-                $tore = 0;
-                $nummer = 0;
-                $sql = "INSERT INTO `tbl_spieler` (`spl_vorname`, `spl_nachname`, `spl_fs_team`, `spl_alter`, `spl_fs_position`, `spl_ausdauer`, `spl_technik`, `spl_torgefahr`, `spl_zweikampf`, `spl_rote`, `spl_gelbe`, `spl_verletzt`, `spl_preis`, `spl_tore`, `spl_nummer`) 
-    VALUES ('spl_" . $i . "_vorname', 'spl_" . $i . "_nachname',' " . $team_id . "','" . $alter . "', '" . $position . "', '" . $ausdauer . "', '" . $technik . "', '" . $torgefahr . "', '" . $zweikampf . "', '0', '0', '0', '" . $preis . "', '1', '" . $i . "');";
-            //INSERT INTO `tbl_spieler` (`spl_id`, `spl_vorname`, `spl_nachname`, `spl_fs_team`, `spl_alter`, `spl_fs_position`, `spl_ausdauer`, `spl_technik`, `spl_torgefahr`, `spl_zweikampf`, `spl_rote`, `spl_gelbe`, `spl_verletzt`, `spl_preis`, `spl_tore`, `spl_nummer`)
-            // VALUES (NULL, 'hans', 'gruber', '15', '35', '1', '100', '100', '100', '100', '0', '0', '0', '1', '1', '1')
-            //echo($sql); echo "<br>";
-            mysqli_query($con, $sql);
-            }
-        }
-    //var_dump($foo->get_all_teamplayers());
-
-    echo "<br><br>...";
-
-    //var_dump($foo->get_fieldnames());
-
-    echo "<br><br>...";
+    include_once "./scripte/php/Fixtures.php";
+    $fixtures = new Fixtures();
+    $fixtures->set_player_data();
 
 ?>

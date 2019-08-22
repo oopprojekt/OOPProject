bd7bd45f0454d7c4b692233d79afeff74b4b7b91
Tobias Kinder <tk.tobiki@gmail.com>
2019-08-23 00:30:32 +0200

    rettungsversuch bildpfad

--- home.php
+++ home.php
@@ -27,7 +27,7 @@ if(!$_SESSION['team'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style/maintemplate.css">
+    <link rel="stylesheet" href="maintemplate.css">
     <title>Start Page</title>
 </head>
 <body>
